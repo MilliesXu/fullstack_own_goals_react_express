3a12d9a006aca986a079b7c0ec3be5d261b8d690
MilliesXu <winzchip@gmail.com>
2022-05-14 09:28:33 +0700

    Add login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>
-      <form>
-        <div>
-          <input type="email" name="email" placeholder='Enter your email address' />
-        </div>
-        <div>
-          <input type="password" name="password" placeholder='Enter your password' />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+    <div className="grid place-items-center h-screen">
+      <div className="w-full md:w-1/2">
+        <form className="shadow-md rounded px-8 pt-6 pb-8 bg-gray-100">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-lg font-bold mb-2" htmlFor="email">Email</label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-400" type="email" name="email" placeholder='Enter your email address' id="email" />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-lg font-bold mb-2" htmlFor="password">Password</label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-400" type="password" name="email" placeholder='Enter your password' id="password" />
+          </div>
+          <div className='flex flex-row items-center justify-between'>
+            <button className="bg-blue-600 text-white p-2 px-5 font-bold text-lg hover:bg-blue-800" type="submit">Login</button>
+            <button className="text-blue-500 p-2 px-5 font-bold text-lg hover:text-blue-800" type="button">Forgot Password?</button>
+          </div>
+          
+        </form>
+      </div>
     </div>
   )
 }
